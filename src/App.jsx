fa8c03c2ef1763d74116ee68edcd8de56a53a273
CoderGirl <angi.adema@gmail.com>
2023-10-11 17:07:43 -0600

    added TodoList.jsx after deleting and practicing, updated this and App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,21 +4,57 @@ import "./style.css";
 
 export default function App() {
   const [todos, setTodos] = useState([]);
-  const [newTodoItem, setnewTodoItem] = useState("");
+  const [newTodo, setnewTodo] = useState("");
 
-  function addTodo() {}
+  function addTodo() {
+    if (newTodo === "") return;
 
-  function deleteTodo() {}
+    setTodos((currTodos) => {
+      return [
+        ...currTodos,
+        { listItem: newTodo, completed: false, id: crypto.randomUUID() },
+      ];
+    });
+    setnewTodo("");
+  }
+
+  function checkedTodo(todoId, completed) {
+    setTodos((currTodos) => {
+      return currTodos.map((todo) => {
+        if (todo.id === todoId) return { ...todo, completed };
+        return todo;
+      });
+    });
+  }
+
+  function deleteTodo(todoId) {
+    setTodos((currTodos) => {
+      return currTodos.filter((todo) => todo.id !== todoId);
+    });
+  }
 
   return (
     <>
+      <ul id="list">
+        {todos.map((todo) => {
+          return (
+            <TodoList
+              key={todo.id}
+              {...todo}
+              deleteTodo={deleteTodo}
+              checkedTodo={checkedTodo}
+            />
+          );
+        })}
+      </ul>
+
       <div id="new-todo-form">
         <label htmlFor="todo-input">New Todo</label>
         <input
           type="text"
           id="todo-input"
-          value={newTodoItem}
-          onChange={(e) => setnewTodoItem(e.target.value)}
+          value={newTodo}
+          onChange={(e) => setnewTodo(e.target.value)}
         />
         <button onClick={addTodo}>Add Todo</button>
       </div>
--- src/TodoList.jsx
+++ src/TodoList.jsx
@@ -1,11 +1,18 @@
-export function TodoList() {
+export function TodoList({ id, listItem, completed, deleteTodo, checkedTodo }) {
   return (
     <li className="list-item">
       <label className="list-item-label">
-        <input type="checkbox" checked={completed} data-list-item-checkbox />
-        <span data-list-item-text>Item 1</span>
+        <input
+          type="checkbox"
+          checked={completed}
+          data-list-item-checkbox
+          onChange={(e) => checkedTodo(id, e.target.checked)}
+        />
+        <span data-list-item-text>{listItem}</span>
       </label>
-      <button data-button-delete>Delete</button>
+      <button onClick={() => deleteTodo(id)} data-button-delete>
+        Delete
+      </button>
     </li>
   );
 }
