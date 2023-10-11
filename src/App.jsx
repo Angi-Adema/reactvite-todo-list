3a929040143428658c1a882421e91beedf4b1cc1
CoderGirl <angi.adema@gmail.com>
2023-10-11 11:54:44 -0600

    added addTodo and deleteTodo functions to App.jsx along with the return html in App.jsx and updated the html code in TodoList.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,27 @@
 import { useState } from "react";
+import { TodoList } from "./TodoList";
 import "./style.css";
 
 export default function App() {
-  return <div>Hi</div>;
+  const [todos, setTodos] = useState([]);
+  const [newTodoItem, setnewTodoItem] = useState("");
+
+  function addTodo() {}
+
+  function deleteTodo() {}
+
+  return (
+    <>
+      <div id="new-todo-form">
+        <label htmlFor="todo-input">New Todo</label>
+        <input
+          type="text"
+          id="todo-input"
+          value={newTodoItem}
+          onChange={(e) => setnewTodoItem(e.target.value)}
+        />
+        <button onClick={addTodo}>Add Todo</button>
+      </div>
+    </>
+  );
 }
--- src/TodoList.jsx
+++ src/TodoList.jsx
@@ -1,9 +1,11 @@
 export function TodoList() {
   return (
-    <li>
-      <input type="checkbox" />
-      <span>Placeholder text here</span>
-      <button>Delete</button>
+    <li className="list-item">
+      <label className="list-item-label">
+        <input type="checkbox" checked={completed} data-list-item-checkbox />
+        <span data-list-item-text>Item 1</span>
+      </label>
+      <button data-button-delete>Delete</button>
     </li>
   );
 }
